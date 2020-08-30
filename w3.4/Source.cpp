268572981f973e00dba5a6cb18ef177bf7f5c4fa
Sirinoot Ketkham(Fukfang) <69237528+sirinoot@users.noreply.github.com>
2020-08-30 20:50:59 +0700

    Add project files.

---
+++ w3.4/Source.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define NAME "KMITL"
+#define _CRT_SECURE_NO_WARNINGS
+int main()
+{
+	const char name[9] = "Sirinoot";
+	printf("%s\n", name);
+	printf("%s", NAME);
+
+	return 0;
+}
